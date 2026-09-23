a99de296c843f51e93517ac9703ff21088a5902e
Mohathis <136694073+Mohathis@users.noreply.github.com>
2026-09-23 12:34:15 +0530

    Add MainActivity2 for displaying user details

---
+++ record10.1/MainActivity2.java
@@ -0,0 +1,45 @@
+package com.example.intent;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView e1, e2,e3;
+    Button bt1;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        e1 = findViewById(R.id.textView);
+        e2 = findViewById(R.id.textView2);
+        e3 = findViewById(R.id.textView3);
+        bt1 = findViewById(R.id.button2);
+        Bundle extra = getIntent().getExtras();
+        String text = extra.getString("name");
+        String text1 = extra.getString("age");
+        String text2 = extra.getString("place");
+
+        e1.setText(text);
+        e2.setText(text1);
+        e3.setText(text2);
+
+    }
+
+    public void Click(View view){
+        Intent i=new Intent(this, MainActivity3.class);
+
+
+        startActivity(i);
+
+    }
+}
+
